96baca0a2efc634fc48b283731e343e6e3457993
David Frank <bitinn@gmail.com>
2015-10-24 18:54:00 +0800

    api doc update

--- components/router/router.js
+++ components/router/router.js
@@ -10,6 +10,7 @@ var api = require('koa-router')();
 var getUserInfo = require('../handlers/get-user-info');
 var getRoomInfo = require('../handlers/get-room-info');
 var getRoomUsers = require('../handlers/get-room-users');
+var verifyRoomCode = require('../handlers/verify-room-code');
 var enterRoom = require('../handlers/enter-room');
 var exitRoom = require('../handlers/exit-room');
 
@@ -31,6 +32,7 @@ function myRouter(app) {
 	});
 
 	api.get('/api/users/:uid', getUserInfo());
+	api.get('/api/rooms/verify', verifyRoomCode());
 	api.get('/api/rooms/:rid', getRoomInfo());
 	api.get('/api/rooms/:rid/users', getRoomUsers());
 	api.put('/api/rooms/:rid/users/:uid', enterRoom());
